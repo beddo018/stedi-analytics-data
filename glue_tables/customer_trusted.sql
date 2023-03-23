2ad4adc48230fca3db1c0776f5657c2bce791e1b
mx6367 <mx6367@engie.com>
2023-03-22 21:59:07 -0500

    add customer_trusted table DDL

---
+++ glue_tables/customer_trusted.sql
@@ -0,0 +1,24 @@
+CREATE EXTERNAL TABLE `customer_trusted`(
+  `serialnumber` string COMMENT 'from deserializer',
+  `sharewithpublicasofdate` bigint COMMENT 'from deserializer',
+  `birthday` string COMMENT 'from deserializer',
+  `registrationdate` bigint COMMENT 'from deserializer',
+  `sharewithresearchasofdate` bigint COMMENT 'from deserializer',
+  `customername` string COMMENT 'from deserializer',
+  `email` string COMMENT 'from deserializer',
+  `lastupdatedate` bigint COMMENT 'from deserializer',
+  `phone` string COMMENT 'from deserializer',
+  `sharewithfriendsasofdate` bigint COMMENT 'from deserializer',
+ROW FORMAT SERDE
+  'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'paths'='serialNumber,shareWithPublicAsOfDate,birthday,registrationDate,shareWithResearchAsOfDate,customerName,email,lastUpdateDate,phone,shareWithFriendsAsOfDate')
+STORED AS INPUTFORMAT
+  'org.apache.hadoop.mapred.TextInputFormat'
+OUTPUTFORMAT
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://stedi-lake-house-srrb/customer/trusted/'
+TBLPROPERTIES (
+  'classification'='json')
+  

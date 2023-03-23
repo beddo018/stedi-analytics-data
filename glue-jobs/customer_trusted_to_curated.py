8913476046407d4534d013a9d16c245158a526df
mx6367 <mx6367@engie.com>
2023-03-22 21:56:39 -0500

    added glue script for curating customer data - only allow those with accelerometer data readings

---
+++ glue-jobs/customer_trusted_to_curated.py
@@ -0,0 +1,68 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+# Script generated for node Customer Trusted Zone
+CustomerTrustedZone_node1679293377762 = glueContext.create_dynamic_frame.from_options(
+    format_options={"multiline": False},
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "paths": ["s3://stedi-lake-house-srrb/customer/trusted/"],
+        "recurse": True,
+    },
+    transformation_ctx="CustomerTrustedZone_node1679293377762",
+)
+
+# Script generated for node Accelerometer Landing
+AccelerometerLanding_node1 = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="accelerometer_landing",
+    transformation_ctx="AccelerometerLanding_node1",
+)
+
+# Script generated for node ApplyMapping
+ApplyMapping_node2 = Join.apply(
+    frame1=AccelerometerLanding_node1,
+    frame2=CustomerTrustedZone_node1679293377762,
+    keys1=["user"],
+    keys2=["email"],
+    transformation_ctx="ApplyMapping_node2",
+)
+
+# Script generated for node Drop Fields
+DropFields_node1679294498877 = DropFields.apply(
+    frame=ApplyMapping_node2,
+    paths=["z", "y", "x", "timestamp", "user"],
+    transformation_ctx="DropFields_node1679294498877",
+)
+
+DropFields_node1679294498877 = DropFields.apply(
+    frame=ApplyMapping_node2,
+    paths=["z", "y", "x", "timestamp", "user"],
+    transformation_ctx="DropFields_node1679294498877",
+)
+
+# Script generated for node Customer Curated
+CustomerCurated_node3 = glueContext.write_dynamic_frame.from_options(
+    frame=DropFields_node1679294498877,
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "path": "s3://stedi-lake-house-srrb/customer/curated/",
+        "partitionKeys": [],
+    },
+    transformation_ctx="CustomerCurated_node3",
+)
+
+job.commit()

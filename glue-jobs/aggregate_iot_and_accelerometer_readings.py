1f3afddb891e6cb5cdd020cd3d15a23a4d8d85d2
mx6367 <mx6367@engie.com>
2023-03-24 00:13:01 -0500

    add screenshot of working query; add comment to aggregation script

--- glue-jobs/aggregate_iot_and_accelerometer_readings.py
+++ glue-jobs/aggregate_iot_and_accelerometer_readings.py
@@ -4,6 +4,16 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue import DynamicFrame
+
+# Aggregates Accelerometer readings along with Step Trainer IoT data on matching timestamps.
+
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -11,13 +21,6 @@ glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
-
-# Script generated for node Step Trainer Trusted
-StepTrainerTrusted_node1 = glueContext.create_dynamic_frame.from_catalog(
-    database="stedi",
-    table_name="step_trainer_trusted",
-    transformation_ctx="StepTrainerTrusted_node1",
-)
 
 # Script generated for node Accelerometer Readings Trusted
 AccelerometerReadingsTrusted_node1679629078364 = (
@@ -28,18 +31,30 @@ AccelerometerReadingsTrusted_node1679629078364 = (
     )
 )
 
-# Script generated for node Match IoT data to Accelerometer readings
-MatchIoTdatatoAccelerometerreadings_node2 = Join.apply(
-    frame1=StepTrainerTrusted_node1,
-    frame2=AccelerometerReadingsTrusted_node1679629078364,
-    keys1=["sensorreadingtime"],
-    keys2=["timestamp"],
-    transformation_ctx="MatchIoTdatatoAccelerometerreadings_node2",
+# Script generated for node Step Trainer Trusted
+StepTrainerTrusted_node1 = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="step_trainer_trusted",
+    transformation_ctx="StepTrainerTrusted_node1",
+)
+
+# Script generated for node aggregate_on_timestamps
+SqlQuery0 = """
+select a.user, s.serialnumber, a.timestamp, a.x, a.y, a.z, s.distancefromobject from s inner join a on (s.sensorreadingtime = a.timestamp) group by user, serialnumber, timestamp, x, y, z, distancefromobject order by timestamp desc;
+"""
+aggregate_on_timestamps_node1679631109950 = sparkSqlQuery(
+    glueContext,
+    query=SqlQuery0,
+    mapping={
+        "a": AccelerometerReadingsTrusted_node1679629078364,
+        "s": StepTrainerTrusted_node1,
+    },
+    transformation_ctx="aggregate_on_timestamps_node1679631109950",
 )
 
 # Script generated for node S3 bucket
 S3bucket_node3 = glueContext.write_dynamic_frame.from_options(
-    frame=MatchIoTdatatoAccelerometerreadings_node2,
+    frame=aggregate_on_timestamps_node1679631109950,
     connection_type="s3",
     format="json",
     connection_options={
